68d1031259eb03ed136dad05dade2262a477d843
celisa <elisa96chen@gmail.com>
2022-09-14 03:47:54 +0000

    adding files

---
+++ mainlib/qabot.py
@@ -0,0 +1,80 @@
+# Importing necessary libraries
+import os
+import random
+import pandas as pd
+import openai
+import click
+#import warnings
+
+
+# Using dask to load and manipulate the data
+from dask import dataframe as dd
+from dask.distributed import Client
+
+# Loading the data
+def load_data():
+    """Load the data from the csv file using kaggle api"""
+    # Setting up the client
+    client = Client()
+    pd_df = pd.read_csv(
+        "./raw_data/cleanposts.csv", nrows=100000
+    )  # REMEMBER TO CHANGE NROWS
+    ddf = dd.from_pandas(pd_df, npartitions=10)
+    return ddf
+
+
+# A Function To Find All Messages With Question Marks
+def find_question_mark(df):
+    """Find all messages with question marks"""
+    df["question_mark"] = df["cleanmessage"].str.contains(r"\?")
+    df = df[df["question_mark"] == True]
+    return df
+
+
+# A Function to extract the question from the message
+def extract_question(df):
+    """Extract the question from the message"""
+    df = df.copy()
+    df["question"] = df['cleanmessage'].str.extract(r"\b([A-Z][^.!]*[?])")[0]
+    return df
+
+
+# Function to generate the answer to any question using OpenAI GPT-3 API
+def ask(question):
+    """Generate an answer to any question using OpenAI GPT-3 API"""
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    start_sequence = "\nAI:"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=question + start_sequence,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n", " Human:", " AI:"],
+    )
+    return response["choices"][0]["text"].strip(" \n")
+
+
+# Function to randomly pick a question from the dataset and provide a response via OpenAI GPT-3 API
+def answer(question):
+    """Randomly pick a question from the dataset and provide a response via OpenAI GPT-3 API"""
+    #warnings.filterwarnings("ignore")
+    print("Question: " + question)
+    print("Answer: " + ask(question))
+
+
+# Function to run the bot and respond to a question. By default it will respond to a random question.
+def return_answer(question=""):
+    """Randomly select a question from the White Power Forum posts and provide an answer using OpenAI GPT-3 API"""
+    if question == "":
+        ddf = load_data()
+        ddf = ddf.map_partitions(find_question_mark)
+        ddf = ddf.map_partitions(extract_question)
+        ddf = ddf.dropna(subset=["question"])
+        index = random.randint(1, len(ddf))
+        question = ddf["question"].compute().tolist()[index]
+    answer(question)
